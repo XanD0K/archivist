c546208723ea31ee34cf011d9be2601271ea59ce
XanD0K <element_trading_47@icloud.com>
2026-03-28 07:59:23 -0300

    Added sort flag on GeneralOptions

--- include/cli_opts.h
+++ include/cli_opts.h
@@ -3,12 +3,14 @@
 
 #include <stdbool.h>
 
+// Structure of all CLI flags shared among files
 typedef struct
 {
     char *extension;
     bool human_readable;
     bool ignore_case;
     bool recursive;
+    char *sort;
 } GeneralOptions;
 
 #endif
