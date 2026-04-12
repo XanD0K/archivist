bd65028be67a5ff7c80fa6ae509595fbab8fe54c
XanD0K <element_trading_47@icloud.com>
2026-04-11 23:06:14 -0300

    Added logic for 'rename' feature

--- include/help.h
+++ include/help.h
@@ -6,6 +6,7 @@ void handle_help(void);
 void print_delete_help(void);
 void print_list_help(void);
 void print_move_help(void);
+void print_rename_help(void);
 void print_report_help(void);
 void print_search_help(void);
 void print_tree_help(void);
