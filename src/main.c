60bae6b747d4270e323a4a71f6df86f89c6589e8
XanD0K <element_trading_47@icloud.com>
2026-04-15 21:37:02 -0300

    Fixed main function output, which nows output an ErrorCode type

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 // Headers
 #include "archivist.h"
 
-ErrorCode main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    return execute_command(argc, argv);
+    return (int)execute_command(argc, argv);
 }
