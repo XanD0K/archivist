af442aadaddce866093eaf9a33ec278fca3c136f
XanD0K <element_trading_47@icloud.com>
2026-05-01 21:21:33 -0300

    Added 'get_size()' function, added flags, improved code, fixed minor bugs

--- src/cli_parse.c
+++ src/cli_parse.c
@@ -1,15 +1,23 @@
 // Libraries
+#include <errno.h>
 #include <getopt.h>
 #include <stdbool.h>
 #include <stddef.h>  // NULL
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <unistd.h>  // optind | optarg
 
 // Headers
 #include "cli_parse.h"
 #include "utils.h"
+#include "utils_filter.h"
 
+// Prototypes
+static off_t get_size(const char *size);
+
+// Checks common flags
 void handle_common_flag(int opt, char *opt_arg, CommonOptions *opts,
                          uint32_t supported_flags)
 {
@@ -39,14 +47,6 @@ void handle_common_flag(int opt, char *opt_arg, CommonOptions *opts,
             }
             break;
         }
-        case 'R':  // recursive
-        {
-            if (supported_flags & COMMON_RECURSIVE)
-            {
-                opts->recursive = true;
-            }
-            break;
-        }
         case 'A': // almost-all
         {
             if (supported_flags & COMMON_ALMOST_ALL)
@@ -56,9 +56,18 @@ void handle_common_flag(int opt, char *opt_arg, CommonOptions *opts,
             }
             break;
         }
+        case 'R':  // recursive
+        {
+            if (supported_flags & COMMON_RECURSIVE)
+            {
+                opts->recursive = true;
+            }
+            break;
+        }
     }
 }
 
+// Checks filter flags
 void handle_filter_flag(int opt, const char *opt_name, char *opt_arg,
                          FilterOptions *opts, uint32_t supported_flags)
 {
@@ -109,6 +118,7 @@ void handle_filter_flag(int opt, const char *opt_name, char *opt_arg,
     }
 }
 
+// Checks action flags
 void handle_action_flag(int opt, ActionOptions *opts, uint32_t supported_flags)
 {
     switch (opt)
@@ -138,4 +148,54 @@ void handle_action_flag(int opt, ActionOptions *opts, uint32_t supported_flags)
             break;
         }
     }
+}
+
+// Converts user's input to off_t size
+static off_t get_size(const char *size)
+{
+    const off_t BUFFER = 1024;
+    char *ptr;
+    errno = 0;
+    long num = strtol(size, &ptr, 10);
+
+    if (errno == EINVAL || errno == ERANGE)
+    {
+        fprintf(stderr, "Error on strtol(): %s\n", strerror(errno));
+        return -1;
+    }
+    if (num < 0)
+    {
+        fprintf(stderr, "Size can't be negative: %ld\n", num);
+        return -1;
+    }
+    if (ptr == size)
+    {
+        fprintf(stderr, "No digits were found!\n");
+        return -1;
+    }
+
+    num = (off_t)num;
+
+    if(ptr == NULL || *ptr == '\0' || strcasecmp(ptr, "B") == 0)
+    {
+        return num;
+    }
+    else if (strcasecmp(ptr, "K") == 0 || strcasecmp(ptr, "KB") == 0)
+    {
+        return num * BUFFER;
+    }
+    else if (strcasecmp(ptr, "M") == 0 || strcasecmp(ptr, "MB") == 0)
+    {
+        return num * BUFFER * BUFFER;
+    }
+    else if (strcasecmp(ptr, "G") == 0 || strcasecmp(ptr, "GB") == 0)
+    {
+        return num * BUFFER * BUFFER * BUFFER;
+    }
+    else if (strcasecmp(ptr, "T") == 0 || strcasecmp(ptr, "TB") == 0)
+    {
+        return num * BUFFER * BUFFER * BUFFER * BUFFER;
+    }
+
+    return -1;
 }
