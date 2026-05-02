ec1928f1cd3918fbfc2db2f6ee61ac9a65f6660e
XanD0K <element_trading_47@icloud.com>
2026-05-01 21:20:10 -0300

    Improved file

--- include/utils.h
+++ include/utils.h
@@ -9,8 +9,8 @@
 #include <sys/stat.h>
 #include <sys/types.h>  // off_t
 
-// Type/Alias
-typedef int (*ScandirFilter)(const struct dirent *);
+// Headers
+#include "cli_opts.h"
 
 // Structure for each extension
 typedef struct
@@ -21,6 +21,7 @@ typedef struct
 } Extension;
 
 // Prototypes
+bool check_help(int argc, const char *argv);
 char *get_valid_directory(const char *path);
 char *get_valid_destination(const char *path);
 int scandir_no_dot_filter(const struct dirent *entry);
@@ -30,20 +31,22 @@ char *formatted_output(off_t total_size);
 const char *get_clean_extension(const char *name);
 Extension *get_all_extensions(const char *exts, size_t *ext_count);
 void free_extensions(Extension *ext, size_t ext_counter);
-bool check_help(int argc, const char *argv);
 const char *get_suffix(const char *path, const char *base_dir);
 bool get_answer(const char *prompt);
 int check_path_name_size(char *dst, size_t len, const char *prefix, const char *suffix);
-bool file_needs_backup(struct stat *st_src, const char *src_dir, const char *dst_dir);
-int copy_file(const char *src_path, const char *dst_path);
-bool check_value_in_list(const char *name, const char *list[], size_t len);
+bool check_value_in_list(const char *name, const char **list, size_t len);
 void get_formatted_time(char *timestamp, size_t len);
 void free_dirent(struct dirent **tmp,int len);
 char *validate_type(const char *type);
 bool is_directory_type (const char *type);
 bool is_slink_type (const char *type);
 bool is_file_type (const char *type);
+bool file_needs_backup(struct stat *st_src, const char *src_dir, const char *dst_dir);
+int copy_file(const char *src_path, const char *dst_path);
 uint32_t crc32(const void *data, size_t len);
 uint32_t quick_file_hash(const char *path);
+void print_divider(void);
+void print_counter_err_msg(size_t counter);
+void print_output_message(const char *action, ActionOptions opts, size_t files);
 
 #endif
